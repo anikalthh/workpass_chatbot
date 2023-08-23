6b90c2e87ec1020db05f6d3918dd47d94f775489
anikalthh <95908881+anikalthh@users.noreply.github.com>
2023-08-23 16:15:02 +0800

    original model

--- app/constants.py
+++ app/constants.py
@@ -11,7 +11,7 @@ import streamlit as st
 load_dotenv(find_dotenv())
 
 DOCUMENT = "Combined_MDW_OCR_16Jun23.pdf"
-MODEL_NAME = 'gpt-3.5-turbo-0301'
+MODEL_NAME = 'gpt-3.5-turbo'
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 TEMPERATURE = 0
 KENDRA_INDEX_ID = os.getenv("KENDRA_INDEX_ID")
